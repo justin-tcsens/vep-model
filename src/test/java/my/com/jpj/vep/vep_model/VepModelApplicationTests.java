a2421989fd9f9904b860ab305a3768873d1bf31c
justin.phua <justin.phua@tcsens.com>
2024-06-26 07:00:26 +0800

    Initial Copy

---
+++ src/test/java/my/com/jpj/vep/vep_model/VepModelApplicationTests.java
@@ -0,0 +1,13 @@
+package my.com.jpj.vep.vep_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VepModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
